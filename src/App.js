4a963afa567451b600b98c5d60f019b3a33bb2ab
Waleedoff <77509215+Waleedoff@users.noreply.github.com>
2022-07-20 16:35:52 +0300

    v3

--- src/App.js
+++ src/App.js
@@ -16,25 +16,25 @@ function App() {
       "error": "Q1:  تحميل رياكت",
       "sol": "باستخدام الامر 'create-react-app'",
       "img": "https://camo.githubusercontent.com/29765c4a32f03bd01d44edef1cd674225e3c906b/68747470733a2f2f63646e2e7261776769742e636f6d2f66616365626f6f6b2f6372656174652d72656163742d6170702f323762343261632f73637265656e636173742e737667",
-      "ref": "react.doucmentation.com"
+      
     },
     {
       "error": "Q2:  تحميل ريكات بوتسراب",
       "sol": "باستخدام الامر  'npm install react-bootstrap bootstrap'",
       "img": "https://www.tutorialandexample.com/wp-content/uploads/2019/12/React-Bootstrap-Installation-2.png",
-      "ref": "react.doucmentation.com"
+      
     },
     {
       "error": "Q3  React Router: كيفية تحميل التوجيه في  ",
       "sol": "باستخدام الامر'npm install react-bootstrap bootstrap'",
       "img": "https://www.arabicprogrammer.com/images/72/313154e8beb9ee34c38603d99d7451a0.png",
-      "ref": "react.doucmentation.com"
+      
     },
     {
       "error": "Q3  React Router: كيفية تحميل التوجيه في  ",
       "sol": "باستخدام الامر'npm install react-bootstrap bootstrap'",
       "img": "https://www.arabicprogrammer.com/images/72/313154e8beb9ee34c38603d99d7451a0.png",
-      "ref": "react.doucmentation.com"
+      
     }
   ]
   // const users = [
@@ -51,7 +51,7 @@ function App() {
   // ]
   return (
     <div className="App" >
-      <Navbarr/>
+  <Navbarr/>
       {/* <Question props = {question}/> */}
 
 
